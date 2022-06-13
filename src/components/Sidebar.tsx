fa882c5f8fb5110627903f4d65dcf4d40b003a21
Justin Souza <justinsouza1029@gmail.com>
2022-06-13 14:04:01 -0700

    Add button to import profile picture

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import './Sidebar.css'
+import {IoMdAdd} from 'react-icons/io'
 
 function Sidebar() {
   return (
     <div className='sidebar-wrapper'> 
       <div className='image-container'> 
-        
+        <button><IoMdAdd/></button>
       </div>
       <button>Chats</button>
       <button>Profile</button>
