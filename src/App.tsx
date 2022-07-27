c11d0dd43cffa125a8f5ab4ddc99e681061b4f12
Justin Souza <justinsouza1029@gmail.com>
2022-07-26 21:49:12 -0700

    Updated styles

--- src/App.tsx
+++ src/App.tsx
@@ -8,25 +8,22 @@ import RequiredAuth from './components/config/RequiredAuth';
 function App() {    
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false)
   const [userData, setUserData] = useState<object>({})
-  // user will be logged out because the default value of isLoggedIn is false.
-  // when the component rerenders is then set to false.
+ 
   return (  
       <div className="App">   
         <Router>  
           <Routes>  
             <Route path='/' element={<EntryPage  
             setIsLoggedIn={setIsLoggedIn} 
-            setUserData={setUserData} 
             isLoggedIn={isLoggedIn}
             /> 
             }>
             </Route>
             <Route path='/dashboard' 
             element={
-              // <RequiredAuth isLoggedIn={isLoggedIn}>
-              //   <Dashboard userData={userData}/>
-              // </RequiredAuth> 
-              <Dashboard userData={userData}/>
+              <RequiredAuth isLoggedIn={isLoggedIn} setUserData={setUserData}>
+                <Dashboard userData={userData} setIsLoggedIn={setIsLoggedIn}/>
+              </RequiredAuth> 
             }/>
           </Routes>
         </Router>
--- src/components/EntryPage.tsx
+++ src/components/EntryPage.tsx
@@ -5,19 +5,18 @@ import './EntryPage.css'
 import {animated, useSpring} from '@react-spring/web'
 
 type EntryPageProps = { 
-  setIsLoggedIn: (a:boolean) => void, 
-  setUserData: (a:Object) => void, 
+  setIsLoggedIn: (a:boolean) => void,  
   isLoggedIn: boolean,
 }
 
-function EntryPage({setIsLoggedIn, setUserData, isLoggedIn}:EntryPageProps) {  
+function EntryPage({setIsLoggedIn, isLoggedIn}:EntryPageProps) {  
   const [form, setForm] = useState<string>('register')   
 
   const slider = useSpring({  
-    config:{ duration: 300},
-    right: form === 'login' ? '0px' : '100px',
+    config:{ duration: 200},
+    right: form === 'login' ? '0px' : '100px', 
     borderRadius: form === 'login' ? '0 12px 12px 0' : '12px 0 0 12px'
-  })
+  }) 
 
   const registerHandler = () => { 
     // toggleButtonAnimation() 
@@ -39,7 +38,6 @@ function EntryPage({setIsLoggedIn, setUserData, isLoggedIn}:EntryPageProps) {
       {form === 'register' && <Register setForm={setForm}/>}
       {form === 'login' && <Login 
       setIsLoggedIn={setIsLoggedIn} 
-      setUserData={setUserData} 
       isLoggedIn={isLoggedIn}
       />}
     </>
