7daca4e3bf4faf8c5ef1652535c60082933897f5
Justin Souza <justinsouza1029@gmail.com>
2022-07-26 21:41:20 -0700

    Removed some state and simplified component logic

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,49 +1,38 @@
-import React, {useState, useEffect} from 'react'
-import {auth} from './config/firebase' 
-import {signInWithEmailAndPassword, onAuthStateChanged} from "firebase/auth";
+import React, {useState} from 'react' 
+import {signInWithEmailAndPassword, getAuth} from "firebase/auth";
 import { useNavigate } from 'react-router-dom';  
 
 type LoginProps = { 
-  setIsLoggedIn: (a:boolean) => void,
-  setUserData: (a:Object) => void,  
+  setIsLoggedIn: (a:boolean) => void, 
   isLoggedIn: boolean,
 }
 
-function Login({setIsLoggedIn, setUserData, isLoggedIn}:LoginProps) { 
+const Login: React.FunctionComponent<LoginProps> = ({setIsLoggedIn}:LoginProps) => {  
   const [email, setEmail] = useState<string>('')
   const [password, setPassword] = useState<string>('') 
   const [loginError, setLoginError] = useState<string>('')
-  // const [isLoading, setIsLoading] = useState<boolean>(true)
 
   const navigate = useNavigate() 
+  const auth = getAuth()
 
   const submitLoginHandler = (event: React.FormEvent<HTMLFormElement>) => { 
-    event.preventDefault() 
-    signInWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => { 
-      const user = userCredential.user; 
-      console.log(user)
+    if(loginError) setLoginError('')
+
+    event.preventDefault()  
+  
+    signInWithEmailAndPassword(auth, email, password) 
+    .then((userCredential) => {  
+      setIsLoggedIn(true)
       navigate('/dashboard')
+      const user = userCredential.user;
     })
     .catch((error) => {
       const errorCode = error.code;
-      const errorMessage = error.message; 
       errorCode === 'auth/wrong-password' && setLoginError('password is incorrect')
-      errorCode === 'auth/user-not-found' && setLoginError('User not found')
+      errorCode === 'auth/user-not-found' && setLoginError('User not found') 
     });
   }    
-
-  useEffect(() => { 
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        setUserData(user) 
-        setIsLoggedIn(true)
-      } else {
-        //user is signed out
-      } 
-    }); 
-  }, [])
-
+  
   return ( 
     <>  
       <form onSubmit={(event) => submitLoginHandler(event)}>  
@@ -71,4 +60,5 @@ function Login({setIsLoggedIn, setUserData, isLoggedIn}:LoginProps) {
   )
 }
 
-export default Login
+export default Login 
+
