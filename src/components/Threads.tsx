d1ffb0bfdb10bfb502cc678184be6c2aa51e6b2c
Justin Souza <justinsouza1029@gmail.com>
2022-07-30 19:54:17 -0700

    Added styles to Threads component

--- src/components/Threads.tsx
+++ src/components/Threads.tsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import './Threads.css'
 
-const Threads = () => {
+const Threads = () => { 
+  const threads = [
+     {user:'Programmer', message: 'Hello World'}, 
+     {user:'Alice', message: 'woof woof woof, woof. WOOF! woof woof- woof.'}, 
+     {user: 'Google', message: 'Congrats! you are hired'}
+  ]  
+
+  const chats = threads.map((chat, i) => { 
+    return( 
+      <div key={i} className='chat-container'> 
+        <h3>{chat.user}</h3> 
+        <p>{chat.message}</p>
+      </div>
+    )
+  })
+
   return (
-    <div>Threads</div>
+    <div className='threads-wrapper'> 
+      {chats}
+      <div className='input-wrapper'> 
+        <input></input>
+      </div>
+    </div>
   )
 } 
 
