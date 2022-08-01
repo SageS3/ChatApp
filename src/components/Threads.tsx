cc9e5db765f0d7891ad925897fb8800ec347234c
Justin Souza <justinsouza1029@gmail.com>
2022-08-01 15:20:28 -0700

    Added container for threads

--- src/components/Threads.tsx
+++ src/components/Threads.tsx
@@ -5,12 +5,22 @@ const Threads = () => {
   const threads = [
      {user:'Programmer', message: 'Hello World'}, 
      {user:'Alice', message: 'woof woof woof, woof. WOOF! woof woof- woof.'}, 
-     {user: 'Google', message: 'Congrats! you are hired'}
+     {user: 'Google', message: 'Congrats! you are hired'}, 
+     {user: 'Google', message: 'Congrats! you are hired'}, 
+    //  {user: 'Google', message: 'Congrats! you are hired'},
+    //  {user: 'Google', message: 'Congrats! you are hired'},
+    //  {user: 'Google', message: 'Congrats! you are hired'},
+    //  {user: 'Google', message: 'Congrats! you are hired'}
   ]  
 
   const chats = threads.map((chat, i) => { 
     return( 
       <div key={i} className='chat-container'> 
+        <div className='edit-chat-button'> 
+          <span/> 
+          <span/>
+          <span/>
+        </div>
         <h3>{chat.user}</h3> 
         <p>{chat.message}</p>
       </div>
@@ -18,8 +28,10 @@ const Threads = () => {
   })
 
   return (
-    <div className='threads-wrapper'> 
-      {chats}
+    <div className='threads-wrapper'>  
+      <div className='chats-container'> 
+        {chats}
+      </div>
       <div className='input-wrapper'> 
         <input></input>
       </div>
