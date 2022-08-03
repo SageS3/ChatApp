66ee12844cc249d04236272e3ecdd7f8da9f6f9a
Justin Souza <justinsouza1029@gmail.com>
2022-08-03 15:30:03 -0700

    Added updateProfile method

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,25 +1,35 @@
-import React from 'react' 
+import React, {useState} from 'react' 
 import './Profile.css'
+import {getAuth, updateProfile} from 'firebase/auth'
 
-type ProfileProps = { 
-  userData:{[key: string]: any},
-}
-
-const Profile = ({userData}:ProfileProps) => { 
-
-  const displayName = () => { 
-    if(userData.displayName === undefined) return ''
-  }
+const Profile: React.FunctionComponent = () => {  
+  const [userName, setUserName] = useState<string>('userName')
+  const auth = getAuth() 
   
+  const updateUserProfile = (e: React.FormEvent<HTMLFormElement>) => {  
+    e.preventDefault()  
+    
+    // updateProfile(auth, {
+    //   displayName: userName
+    // }).then(() => {
+    //   console.log('profile updated')
+    // }).catch((error) => {
+    //   console.log(error)
+    // });
+  }
+ 
   return (
-    <div className='profile-wrapper'>
+    <form onSubmit={(e) => updateUserProfile(e)} className='profile-wrapper' >
       <div className='profile-photo'/> 
       <div className='displayname-container'>
         <h3>Display name</h3>  
-        <input type='text'>{displayName()}</input>
-      </div> 
-      
-    </div>
+        <input 
+          type='text' 
+          value={userName} 
+          onChange={(e) => setUserName(e.target.value)}
+        ></input>
+      </div>
+    </form>
   )
 } 
 
