d7fb22d5a6d4b6ca6d55df42983b39eaa2011012
Justin Souza <justinsouza1029@gmail.com>
2022-08-17 00:54:31 -0700

    Add global state, useEffect hook and a function that updates the users information

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -1,14 +1,51 @@
 import {useState, useEffect} from 'react'
 import Sidebar from '../dashboard/Sidebar'
-import './Dashboard.css'
-import {signOut, getAuth} from 'firebase/auth'
+import {signOut, getAuth, updateProfile} from 'firebase/auth'
 import Profile from '../Profile'
-import Threads from '../Threads'
+import Threads from '../Threads' 
+import './Dashboard.css'
 
-function Dashboard() { 
-  const [dashboard, setDashboard] = useState<string>('chats')
+//navigate to directories
+const navigateTo = {
+  chats: 'chats',
+  profile: 'profile'
+}
+
+function Dashboard() {
+  const [dashboard, setDashboard] = useState<string>(navigateTo.chats)
   const [userName, setUserName] = useState<string | null>('')
-  const auth = getAuth()   
+  const [userEmail, setUserEmail] = useState<any>('')
+  const [updating, setUpdating] = useState<boolean>(false)
+  // const [password, setPassword] = useState('')
+
+  const auth = getAuth()
+  const user = auth.currentUser  
+  
+  // onComponentDidMount update the user state
+  useEffect(() => { 
+    if(user){  
+      setUserName(user.displayName) 
+      setUserEmail(user.email) 
+    } 
+  },[])  
+
+  const updateUserProfile = async (e: React.FormEvent<HTMLFormElement>) => {  
+    e.preventDefault() 
+    const auth = getAuth()  
+    const user = auth.currentUser
+    // if user is signed in continue to update...
+    if(user){  
+      setUpdating(true)
+      await updateProfile(user, {displayName: userName})
+      .then(() => { 
+        setUserName(user.displayName)
+      }) 
+      .catch((error) => { 
+        console.error(error)
+      })
+    } 
+    setUpdating(false)
+  }
 
   const handleLogOut = () => { 
     signOut(auth) 
@@ -18,21 +55,32 @@ function Dashboard() {
     .catch(error => {  
       console.log(error)
     })
-  }    
+  }  
 
   return (
-    <div className='user-dashboard'> 
+    
+    <div className='user-dashboard'>   
       <nav> 
-        <button onClick={() => setDashboard('chats')}>Chats</button>
-        <button onClick={() => setDashboard('profile')}>Profile</button>
+        <button onClick={() => setDashboard(navigateTo.chats)}>Chats</button>
+        <button onClick={() => setDashboard(navigateTo.profile)}>Profile</button>
         <button>Settings</button>
-        <button onClick={handleLogOut}>Logout</button> 
+        <button className='logout-nav-button' 
+        onClick={handleLogOut}>Logout</button> 
       </nav>
-      <Sidebar 
-        setDashboard={setDashboard}
+      <Sidebar  
+        setDashboard={setDashboard} 
+        userName={userName}
       ></Sidebar> 
-      <main> 
-        {dashboard === 'profile' && <Profile/>} 
+      <main>  
+        {dashboard === 'profile' && 
+        <Profile 
+        userName={userName} 
+        userEmail={userEmail}
+        setUserName={setUserName}
+        updateUser={updateUserProfile}  
+        setUserEmail={setUserEmail}
+        isUpdating={updating}
+        />} 
         {dashboard === 'chats' && <Threads/>}
       </main>  
     </div>
