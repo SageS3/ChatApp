6b75d29844078410837fbf47745d870311e8526d
Justin Souza <justinsouza1029@gmail.com>
2022-09-21 12:59:04 -0700

    Added create userName function after user signs up

--- src/components/entrypage/Register.tsx
+++ src/components/entrypage/Register.tsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react'
-import {auth} from '../config/firebase'
-import { createUserWithEmailAndPassword } from 'firebase/auth' 
-import './Register.css'   
+import React, {useState } from 'react'
+import { addDoc, collection} from 'firebase/firestore';
+import {db} from '../../components/config/firebase'
+import { createUserWithEmailAndPassword, getAuth, updateProfile } from 'firebase/auth' 
+import './Register.css'  
 
 type registerProps = { 
-  setForm: React.Dispatch<React.SetStateAction<string>>
+  setForm: React.Dispatch<React.SetStateAction<string>>, 
 }
 
 export default function Register(props: registerProps){  
@@ -12,15 +13,44 @@ export default function Register(props: registerProps){
   const [password, setPassword] = useState<string>('')
   const [registerError, setRegisterError] = useState<string>(''); 
   const [confirmPassword, setConfirmPassword] = useState<string>('');  
+  const usersCollection = collection(db, 'users')
+
+  const auth = getAuth()
+  
+  const createDefaultUsername = async (userName:string) => { 
+    const user = auth.currentUser 
+    if(user){ 
+      await updateProfile(user, {displayName: userName}) 
+      .then(() => { 
+        console.log('updated username')
+      })
+    }
+  } 
+
+  const sliceEmail = (email:string) => {
+    const atSymbolIndex = email.indexOf('@')
+    const defaultUserName = email.slice(0,atSymbolIndex) 
+    return defaultUserName
+  }
+  
+  const addUser = async (email:string) => {  
+     const userName = sliceEmail(email) 
+     createDefaultUsername(userName)
+    const userInfo = await addDoc(usersCollection, { 
+      userName: userName, 
+      photoURL: 'photourl' 
+    })  
+    console.log(userInfo)
+    console.log('user added to users collection')
+  }
 
   const submitSignUpHandler = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()  
-    
     if(registerError !== '') setRegisterError('') 
     if(password !== confirmPassword) setRegisterError('Password does not match')
-
     createUserWithEmailAndPassword(auth, email, password) 
-    .then((userCredential) => { 
+    .then((userCredential) => {
+      addUser(email) 
       props.setForm('login')
       const user = userCredential.user 
     }) 
@@ -30,11 +60,9 @@ export default function Register(props: registerProps){
       if (errorMessage.includes('auth/weak-password')){ 
         setRegisterError('Enter stronger password') 
       }  
-      
       if(errorMessage.includes('auth/email-already-in-use')){ 
         setRegisterError('Email already in use')
       } 
-
       if(errorMessage.includes('auth/invalid-email')){ 
         setRegisterError('Invalid email') 
       } 
