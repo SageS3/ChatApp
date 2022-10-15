7add6666004c784179a804286bc41866215a059a
Justin Souza <justinsouza1029@gmail.com>
2022-10-14 19:32:46 -0700

    Added auth from import

--- src/components/entrypage/Login.tsx
+++ src/components/entrypage/Login.tsx
@@ -1,14 +1,13 @@
 import React, {useState} from 'react' 
-import {signInWithEmailAndPassword, getAuth} from "firebase/auth";
+import {signInWithEmailAndPassword} from "firebase/auth";
 import { useNavigate } from 'react-router-dom';  
-
+import {auth} from '../config/firebase'
 const Login: React.FunctionComponent = () => {  
   const [email, setEmail] = useState<string>('')
   const [password, setPassword] = useState<string>('') 
   const [loginError, setLoginError] = useState<string>('')
 
   const navigate = useNavigate() 
-  const auth = getAuth()
 
   const submitLoginHandler = (event: React.FormEvent<HTMLFormElement>) => { 
     if(loginError) setLoginError('')
@@ -19,7 +18,6 @@ const Login: React.FunctionComponent = () => {
     .then((userCredential) => {  
       navigate('/dashboard')
       const user = userCredential.user;
-      //
       console.log(user)
     })
     .catch((error) => {
@@ -53,7 +51,6 @@ const Login: React.FunctionComponent = () => {
         <button type='submit'>Login</button> 
         <h4>forgot password?</h4>
       </form>
-     
     </>
   )
 }
