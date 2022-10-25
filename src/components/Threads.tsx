25da8faf552dc3821a007c1e2a40b43751e2575b
Justin Souza <justinsouza1029@gmail.com>
2022-10-25 12:22:54 -0700

    Added text area and added styles to it

--- src/components/Threads.tsx
+++ src/components/Threads.tsx
@@ -1,40 +1,18 @@
-import {useState, useEffect} from 'react' 
-import {db} from '../components/config/firebase'
-import {collection,getDocs,query,limit,onSnapshot} from "firebase/firestore"; 
+import { useEffect} from 'react' 
 import './Threads.css' 
 
 const Threads = () => {  
-  const [userMessages, setUserMessages] = useState<string[]>([])
-  const messagesRef = collection(db, 'messages')
-  const q = query(collection(db, 'messages'), limit(10))
- 
-  const getMessages = async () => {
-    const {docs} = await getDocs(q)  
-    
-  }   
-
-  const mapMessages = () => { 
-    return( 
-      userMessages.map((message, i) => ( 
-        <div key={i}> 
-          {message}
-        </div>
-      ))
-    )
-  }
+  // const [userMessages, setUserMessages] = useState<{}[]>([])
 
   useEffect(() => { 
-    getMessages()
-  }, [])
+    console.log('threads component mounted') 
+  },[])
 
   return (
     <div className='threads-wrapper'>  
-      <div className='chats-container'> 
-      {mapMessages()}
-      </div>
-      <div className='input-wrapper'> 
-        <input></input>
-      </div>
+     <div className='input_container'> 
+      <textarea placeholder='Type here...'></textarea>
+     </div>
     </div>
   )
 } 
