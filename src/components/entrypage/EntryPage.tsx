9dcb91cb4df051266df87232a0588d3977f4e2a8
Justin Souza <justinsouza1029@gmail.com>
2022-10-28 09:57:36 -0700

    Deleted on click handlers, setForm is inside onClick event

--- src/components/entrypage/EntryPage.tsx
+++ src/components/entrypage/EntryPage.tsx
@@ -4,31 +4,24 @@ import Login from './Login'
 import './EntryPage.css'   
 import {animated, useSpring} from '@react-spring/web'
 
-function EntryPage() {  
-  const [form, setForm] = useState<string>('register')   
 
+const EntryPage = () => {  
+  const [form, setForm] = useState<string>('register')   
+ 
   const slider = useSpring({  
     config:{ duration: 200},
     right: form === 'login' ? '0px' : '100px', 
     borderRadius: form === 'login' ? '0 12px 12px 0' : '12px 0 0 12px'
   }) 
 
-  const registerHandler = () => { 
-    setForm('register')
-  } 
-
-  const loginHandler = () => { 
-    setForm('login')
-  }
-
   return (
     <>
       <div className='toggle'>  
         <animated.span style={slider} className='slider'></animated.span>
-        <button type='button' onClick={registerHandler}>Sign Up</button> 
-        <button type='button' onClick={loginHandler}>Sign In</button>
+        <button type='button' onClick={() => setForm('register')}>Sign Up</button> 
+        <button type='button' onClick={() => setForm('login')}>Sign In</button>
       </div> 
-      {form === 'register' && <Register setForm={setForm}/>}
+      {form === 'register' && <Register setForm={setForm} />}
       {form === 'login' && <Login/>}
     </>
   )
