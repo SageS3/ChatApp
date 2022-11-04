6e060f1fbe9fbb7f9f00b59ab10bd1508444717c
Justin Souza <justinsouza1029@gmail.com>
2022-11-03 23:13:26 -0700

    Created a controlled component with password state and added a submit form handler

--- src/components/editProfileForms/SetNewPassword.tsx
+++ src/components/editProfileForms/SetNewPassword.tsx
@@ -1,19 +1,45 @@
-import {useEffect} from 'react'
+import {useEffect, useState} from 'react'
+import { updatePassword} from 'firebase/auth' 
+import {auth} from '../config/firebase'
 import './SetNewPassword.css'
 type SetNewPasswordProps = { 
   setIsSettingPassword: (a:boolean) => void
 }
 const SetNewPassword = ({setIsSettingPassword}:SetNewPasswordProps) => { 
+  const [newPassword, setNewPassword] = useState('') 
+  const [confirmNewPassword, setConfirmNewPassword] = useState('')
 
   useEffect(() => { 
     console.log('set new password form')
-  },[])
+  },[])  
+
+  const user = auth.currentUser
+
+  const handleFormSubmit = () => { 
+    if(confirmNewPassword != newPassword) console.log('password is different')
+    if(user) { 
+      updatePassword(user, confirmNewPassword) 
+      .then(() => { 
+        console.log('password updated')
+      })
+    }
+  }
   return (
     <div className='password-modal-container'> 
-      <form className='password-modal'> 
+      <form className='password-modal' onSubmit={() => handleFormSubmit()}> 
         <h3>Set Password</h3>
-        <input type="text" placeholder="New password"/>
-        <input type="text" placeholder="Confirm password"/> 
+        <input  
+          type="password"  
+          placeholder="New password" 
+          value={newPassword} 
+          onChange={ event => setNewPassword(event.target.value)}
+        />
+        <input  
+          type="password"  
+          placeholder="Confirm password"  
+          value={confirmNewPassword} 
+          onChange={ event => setConfirmNewPassword(event.target.value)}
+        />
         <button type='submit'>confirm</button>
         <button type='button' onClick={() => setIsSettingPassword(false)}>cancel</button>
       </form>  
