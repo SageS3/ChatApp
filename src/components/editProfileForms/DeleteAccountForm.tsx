af25c339d59683fc593528e2ee6fcf1f20b8c4ba
Justin Souza <justinsouza1029@gmail.com>
2022-11-05 11:49:21 -0700

    Added onSubmit function that deletes user with deleteUser method from firebase auth

--- src/components/editProfileForms/DeleteAccountForm.tsx
+++ src/components/editProfileForms/DeleteAccountForm.tsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React from 'react' 
+import ReauthForm from './ReauthForm'
+import {deleteUser } from "firebase/auth";
+import {auth} from '../config/firebase'
 
-const DeleteAccountForm = () => {
+const DeleteAccountForm = () => {  
+  const user = auth.currentUser
+  const handleDeleteAccount = async (event:any) => {  
+    event.preventDefault()
+    if(user) { 
+      await deleteUser(user) 
+      .then(() => { 
+        console.log('User Account Successfully Deleted')
+      }) 
+      .catch((error) => { 
+        console.log(error)
+      })
+    }
+  }
   return (
-    <div>DeleteAccountForm</div>
+    <div className="delete-account-form-container"> 
+      <form onSubmit={(event) => handleDeleteAccount(event)}>  
+        <p>Are you sure you want to do this?</p>
+        <button type="submit">Delete Account</button>
+      </form>
+    </div>
   )
 }
 
