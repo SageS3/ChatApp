341bea9c078ef6f793917837399e52c4a9a04a93
Justin Souza <justinsouza1029@gmail.com>
2022-11-04 23:30:49 -0700

    Added current password input and imported reauth with credential from firebase auth

--- src/components/editProfileForms/SetNewPassword.tsx
+++ src/components/editProfileForms/SetNewPassword.tsx
@@ -1,6 +1,7 @@
 import {useEffect, useState} from 'react'
-import { updatePassword} from 'firebase/auth' 
-import {auth} from '../config/firebase'
+import { updatePassword,reauthenticateWithCredential} from 'firebase/auth' 
+import {auth} from '../config/firebase' 
+
 import './SetNewPassword.css'
 type SetNewPasswordProps = { 
   setIsSettingPassword: (a:boolean) => void
@@ -8,7 +9,8 @@ type SetNewPasswordProps = {
 const SetNewPassword = ({setIsSettingPassword}:SetNewPasswordProps) => { 
   const [newPassword, setNewPassword] = useState('') 
   const [confirmNewPassword, setConfirmNewPassword] = useState('') 
-  const [formError, setFormError] = useState('')
+  const [formError, setFormError] = useState('') 
+  const [currentPassword, setCurrentPassword] = useState('')
 
   useEffect(() => { 
     console.log('set new password form')
@@ -19,26 +21,52 @@ const SetNewPassword = ({setIsSettingPassword}:SetNewPasswordProps) => {
   const handleFormSubmit = (e: any) => {  
     e.preventDefault()
     if(confirmNewPassword != newPassword) setFormError('Passwords not matching')
-    if(user) { 
+    if(user) {  
+
       updatePassword(user, confirmNewPassword) 
       .then(() => { 
         console.log('password updated')
       })
     }
-  }
+  } 
+
+  // const credentials = EmailAuthProvider.credential(reauthEmail, reauthPassword)
+
+  // const reauthUser = async (event:any) => { 
+  //   event.preventDefault()
+  //   if(user){ 
+  //     await reauthenticateWithCredential(user, credentials )
+  //     .then(() => {
+  //       console.log('user reauthenticated')
+  //       setAuthorizing(false)
+  //     }).catch((error) => { 
+  //       console.log(error.message)
+  //       setReauthError('incorrect email or password')
+  //     });
+  //   } 
+  //   setReauthEmail('')
+  //   setReauthPassword('')
+  // } 
+
   return (
     <div className='password-modal-container'> 
       <form className='password-modal' onSubmit={(e) => handleFormSubmit(e)}> 
-        <h3>Set Password</h3>
+        <h3>Set Password</h3> 
         <input  
           type="password"  
-          placeholder="New password" 
+          placeholder="Current Password" 
+          value={currentPassword} 
+          onChange={ event => setCurrentPassword(event.target.value)}
+        />
+        <input  
+          type="password"  
+          placeholder="New Password" 
           value={newPassword} 
           onChange={ event => setNewPassword(event.target.value)}
         />
         <input  
           type="password"  
-          placeholder="Confirm password"  
+          placeholder="Confirm New Password"  
           value={confirmNewPassword} 
           onChange={ event => setConfirmNewPassword(event.target.value)}
         />
