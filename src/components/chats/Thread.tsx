cf4b3366f76aedfaa6d10ffef7c62744b504d850
Justin Souza <justinsouza1029@gmail.com>
2023-02-26 10:30:13 -0800

    Added chat window and input component

--- src/components/chats/Thread.tsx
+++ src/components/chats/Thread.tsx
@@ -1,8 +1,14 @@
 import React from "react"
 import "./Thread.css"
+import Input from "./Input"
 
 const Thread = () => {
-  return <div className="thread-directory">Thread</div>
+  return (
+    <div className="thread-directory">
+      <section className="chat-window">window</section>
+      <Input />
+    </div>
+  )
 }
 
 export default Thread
