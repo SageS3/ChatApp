75d6ac83c5f0d7fcd61fb5be4bbac9e8435608d2
Justin Souza <justinsouza1029@gmail.com>
2023-03-03 11:23:37 -0800

    Added controlled input to change group name

--- src/components/chats/Thread.tsx
+++ src/components/chats/Thread.tsx
@@ -1,11 +1,24 @@
-import React from "react"
+import { useState } from "react"
+import { editGroupName } from "../config/createChat"
 import "./Thread.css"
 import Input from "./Input"
 
 const Thread = () => {
+  const [groupName, setGroupName] = useState<string>("")
+  const handleGroupName = (event: any) => {
+    event.preventDefault()
+    setGroupName(event.target.value)
+    // editGroupName(groupId, groupName)
+  }
   return (
     <div className="thread-directory">
-      <nav className="thread-nav"></nav>
+      <nav className="thread-nav">
+        <input
+          placeholder="group name"
+          value={groupName}
+          onChange={(event) => handleGroupName(event)}
+        ></input>
+      </nav>
       <section className="chat-window">window</section>
       <Input />
     </div>
