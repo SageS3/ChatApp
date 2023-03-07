bf2ac07d2e1b71c070264eab525fc98fa2986d6b
Justin Souza <justinsouza1029@gmail.com>
2023-03-06 23:54:53 -0800

    Added component that returns a list of groups in the DOM tree

--- src/components/chats/Threads.tsx
+++ src/components/chats/Threads.tsx
@@ -1,23 +1,45 @@
 import { useEffect, useState } from "react"
 import "./Threads.css"
-import { getDocs, collection } from "firebase/firestore"
+import { getDocs, collection, where, query } from "firebase/firestore"
 import { db } from "../config/firebase"
+import { auth } from "../config/firebase"
 
 const Threads = () => {
-  const [groups, setGroups] = useState<{}[]>([])
-
-  const listGroups = async () => {
-    const querySnapshot = await getDocs(collection(db, "chat"))
+  const [groups, setGroups] = useState<any>([])
+  const user = auth.currentUser
+  const queryGroups = async () => {
+    const q = query(
+      collection(db, "chat"),
+      where("members", "array-contains", user?.displayName)
+    )
+    const groupArr: any = []
+    const querySnapshot = await getDocs(q)
     querySnapshot.forEach((doc) => {
-      setGroups((prev) => [...prev, doc.data()])
+      groupArr.push(doc.data())
     })
-    console.log(groups)
+    setGroups((prev: any) => [prev, ...groupArr])
   }
 
+  const ListGroups = () => (
+    <>
+      {groups.map((group: any) => (
+        <div className="group-thread" key={group.id}>
+          <p>{group.createdBy}</p>
+        </div>
+      ))}
+    </>
+  )
+
   useEffect(() => {
-    listGroups()
+    queryGroups()
+
+    console.log(groups)
   }, [])
-  return <div className="threads"></div>
+  return (
+    <div className="threads">
+      <ListGroups />
+    </div>
+  )
 }
 
 export default Threads
