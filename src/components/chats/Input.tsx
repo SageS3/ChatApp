e42830b97f85839e81ad5e4fe747af5801b31988
Justin Souza <justinsouza1029@gmail.com>
2023-03-17 22:46:39 -0700

    Added input tag to dom tree

--- src/components/chats/Input.tsx
+++ src/components/chats/Input.tsx
@@ -1,7 +1,11 @@
 import React from "react"
 import "./Input.css"
 const Input = () => {
-  return <div className="input">Input</div>
+  return (
+    <div className="input">
+      <input placeholder="type..."></input>
+    </div>
+  )
 }
 
 export default Input
