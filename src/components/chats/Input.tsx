abe562024b98afe51a8f620d66d9a03ae1ced133
Justin Souza <justinsouza1029@gmail.com>
2023-04-16 23:35:02 -0700

    Added controlled input, state, and a submit handler

--- src/components/chats/Input.tsx
+++ src/components/chats/Input.tsx
@@ -1,9 +1,33 @@
-import React from "react"
+import { useState } from "react"
+import { addMessageSubCollection } from "../config/threadFunctions"
 import "./Input.css"
-const Input = () => {
+
+type InputProps = {
+  threadObj: any
+}
+const Input = ({ threadObj }: InputProps) => {
+  const [messageText, setMessageText] = useState<string>("")
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+
+  const handleInputSubmit = () => {
+    setIsLoading(true)
+    addMessageSubCollection(threadObj.id, messageText).then(() => {
+      setMessageText("")
+    })
+    setIsLoading(false)
+  }
   return (
     <div className="input">
-      <input placeholder="type..."></input>
+      <input
+        type="text"
+        placeholder="type..."
+        value={messageText}
+        spellCheck={true}
+        onChange={(event) => setMessageText(event.target.value)}
+      />
+      <button type="button" onClick={() => handleInputSubmit()}>
+        send
+      </button>
     </div>
   )
 }
