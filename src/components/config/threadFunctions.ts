163bae7be4a59a225d2a4c715eebf1c4a760d942
Justin Souza <justinsouza1029@gmail.com>
2023-04-16 23:44:02 -0700

    Added function that adds subcollection to the message collection with document that is populated with message data

--- src/components/config/threadFunctions.ts
+++ src/components/config/threadFunctions.ts
@@ -1,4 +1,11 @@
-import { addDoc, collection, updateDoc, doc, setDoc } from "firebase/firestore"
+import {
+  addDoc,
+  collection,
+  updateDoc,
+  doc,
+  setDoc,
+  serverTimestamp,
+} from "firebase/firestore"
 import { db } from "./firebase"
 import { auth } from "./firebase"
 
@@ -26,6 +33,21 @@ export const addGroup = async () => {
 
 const addMessageDoc = async (threadId: string) => {
   await setDoc(doc(db, "message", threadId), {})
+}
+
+export const addMessageSubCollection = async (
+  docId: string,
+  message: string
+) => {
+  const user = auth.currentUser
+  const subCollectionRef = collection(db, "message", docId, "messages")
+  await addDoc(subCollectionRef, {
+    sentBy: user?.uid,
+    sentAt: serverTimestamp(),
+    message: message,
+  }).catch((error) => {
+    console.log(error)
+  })
 }
 
 export const editGroupName = async (groupId: string, groupName: string) => {
