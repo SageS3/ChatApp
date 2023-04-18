068441478f25309d5abcdfe09f571a336e522549
Justin Souza <justinsouza1029@gmail.com>
2023-04-17 23:52:37 -0700

    Added function that queries chats

--- src/components/chats/Thread.tsx
+++ src/components/chats/Thread.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react"
 import "./Thread.css"
 import Input from "./Input"
 import { editGroupName } from "../config/threadFunctions"
+import { query, collection, getDocs, limit } from "firebase/firestore"
+import { db, auth } from "../config/firebase"
 
 type ThreadProps = {
   threadObj: any
@@ -9,6 +11,8 @@ type ThreadProps = {
 
 const Thread = ({ threadObj }: ThreadProps) => {
   const [threadGroupName, setThreadGroupName] = useState<string>("")
+  const [chats, setChats] = useState<any>([])
+  const user = auth.currentUser
   const handleGroupName = (event: any) => {
     event.preventDefault()
     setThreadGroupName(event.target.value)
@@ -16,7 +20,22 @@ const Thread = ({ threadObj }: ThreadProps) => {
 
   useEffect(() => {
     setThreadGroupName(threadObj.groupName)
+    queryChats()
   }, [])
+
+  const queryChats = async () => {
+    const q = query(
+      collection(db, `message/${threadObj.id}/messages`),
+      limit(20)
+    )
+    const chats: any = []
+    const querySnapshot = await getDocs(q)
+    querySnapshot.forEach((doc) => {
+      chats.push(doc.data())
+    })
+    setChats(chats)
+  }
+
   return (
     <div className="thread-directory">
       <nav className="thread-nav">
@@ -31,7 +50,7 @@ const Thread = ({ threadObj }: ThreadProps) => {
           onClick={() => editGroupName(threadObj.id, threadGroupName)}
         ></button>
       </nav>
-      <section className="chat-window">window</section>
+      <section className="chat-window">{/* {<ListChats/>} */}</section>
       <Input threadObj={threadObj} />
     </div>
   )
