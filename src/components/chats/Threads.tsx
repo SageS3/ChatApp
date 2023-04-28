1205a44b28fe690a0a3a1232fa92973b04c24729
Justin Souza <justinsouza1029@gmail.com>
2023-04-27 23:07:55 -0700

    Add more elements to the ListThreads component tree as well as a function that handles a slide animation to delete thread

--- src/components/chats/Threads.tsx
+++ src/components/chats/Threads.tsx
@@ -7,19 +7,23 @@ import {
   query,
   doc,
   deleteDoc,
+  orderBy,
 } from "firebase/firestore"
 import { db } from "../config/firebase"
 import { auth } from "../config/firebase"
-import { AiOutlineDelete } from "react-icons/ai"
+import { BsThreeDotsVertical } from "react-icons/bs"
 
 type ThreadsProps = {
   setDashboard: (a: string) => void
-  setThreadObj: (a: string | null) => void
+  setThreadObj: (a: Object | null) => void
 }
 
 const Threads = ({ setDashboard, setThreadObj }: ThreadsProps) => {
   const [groups, setGroups] = useState<any>([])
   const [loading, setLoading] = useState<boolean>(false)
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+  const [clickedItemId, setclickedItemId] = useState<string | null>(null)
+
   const user = auth.currentUser
 
   // the function below gets all of the user's chat threads and stores
@@ -28,6 +32,7 @@ const Threads = ({ setDashboard, setThreadObj }: ThreadsProps) => {
     const q = query(
       collection(db, "chat"),
       where("members", "array-contains", user?.uid)
+      // orderBy("createdAt", "desc")
     )
     const groupArr: any = []
     const querySnapshot = await getDocs(q)
@@ -38,6 +43,7 @@ const Threads = ({ setDashboard, setThreadObj }: ThreadsProps) => {
   }
 
   const deleteThread = async (event: any, groupId: string) => {
+    setIsOpen(false)
     setLoading(true)
     event.stopPropagation()
     await deleteDoc(doc(db, "chat", groupId)).then(queryGroups)
@@ -45,9 +51,16 @@ const Threads = ({ setDashboard, setThreadObj }: ThreadsProps) => {
     setLoading(false)
   }
 
-  const handleThreadId = (groupID: string) => {
+  const handleMenuSlide = (event: any, id: string) => {
+    event.stopPropagation()
+    setclickedItemId(id)
+    setIsOpen(!isOpen)
+  }
+
+  const handleThreadId = (group: Object) => {
+    console.log(group)
+    setThreadObj(group)
     setDashboard("chat")
-    setThreadObj(groupID)
   }
 
   const ListThreads = () => (
@@ -58,14 +71,33 @@ const Threads = ({ setDashboard, setThreadObj }: ThreadsProps) => {
           key={group.id}
           onClick={() => handleThreadId(group)}
         >
+          <p className="thread__last-message-text">
+            {/* {group.recentMessage.readBy.sentAt} */}
+          </p>
+          <p className="thread__group-name">{group.groupName}</p>
+          {group.recentMessage.messageText === null ? (
+            <p>No message yet...</p>
+          ) : (
+            <p>{group.recentMessage.messageText}</p>
+          )}
           <button
             disabled={loading}
             type="button"
-            className="delete-thread"
-            onClick={(e) => deleteThread(e, group.id)}
+            className={isOpen ? "thread-menu open" : "thread-menu"}
+            onClick={(e) => handleMenuSlide(e, group.id)}
           >
-            <AiOutlineDelete size={"2em"} color={"rgb(205, 0, 80)"} />
+            <BsThreeDotsVertical size={"3em"} color={"rgb(77, 255, 148)"} />
           </button>
+          {clickedItemId === group.id && (
+            <ul
+              className={
+                isOpen === true ? "delete-button" : "delete-button closed"
+              }
+              onClick={(e) => deleteThread(e, group.id)}
+            >
+              <li>delete</li>
+            </ul>
+          )}
         </div>
       ))}
     </div>
