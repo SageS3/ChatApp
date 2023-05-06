d23df2aaeb3073938141f5d986afa6dd2a3c4f51
Justin Souza <justinsouza1029@gmail.com>
2023-05-05 23:49:14 -0700

    Add animation on button font color using useSpring

--- src/components/entrypage/EntryPage.tsx
+++ src/components/entrypage/EntryPage.tsx
@@ -1,28 +1,49 @@
-import {useState} from 'react'
-import Register from '../entrypage/Register'
-import Login from './Login' 
-import './EntryPage.css'   
-import {animated, useSpring} from '@react-spring/web'
+import { useState } from "react"
+import Register from "../entrypage/Register"
+import Login from "./Login"
+import "./EntryPage.css"
+import { animated, useSpring } from "@react-spring/web"
 
+const EntryPage = () => {
+  const [form, setForm] = useState<string>("register")
 
-const EntryPage = () => {  
-  const [form, setForm] = useState<string>('register')   
- 
-  const slider = useSpring({  
-    config:{ duration: 200},
-    right: form === 'login' ? '0px' : '100px', 
-    borderRadius: form === 'login' ? '0 12px 12px 0' : '12px 0 0 12px'
-  }) 
+  const slider = useSpring({
+    config: { duration: 200 },
+    right: form === "login" ? "0px" : "100px",
+    borderRadius: form === "login" ? "0 12px 12px 0" : "12px 0 0 12px",
+  })
+
+  const fontColorChange = useSpring({
+    config: { duration: 200 },
+    color: form === "login" ? "black" : "rgb(77, 255, 148)",
+  })
+
+  const registerFontColorChange = useSpring({
+    config: { duration: 200 },
+    color: form === "register" ? "black" : "rgb(77, 255, 148)",
+  })
 
   return (
     <>
-      <div className='toggle'>  
-        <animated.span style={slider} className='slider'></animated.span>
-        <button type='button' onClick={() => setForm('register')}>Sign Up</button> 
-        <button type='button' onClick={() => setForm('login')}>Sign In</button>
-      </div> 
-      {form === 'register' && <Register setForm={setForm} />}
-      {form === 'login' && <Login/>}
+      <div className="toggle">
+        <animated.span style={slider} className="slider"></animated.span>
+        <animated.button
+          type="button"
+          onClick={() => setForm("register")}
+          style={registerFontColorChange}
+        >
+          Sign Up
+        </animated.button>
+        <animated.button
+          type="button"
+          onClick={() => setForm("login")}
+          style={fontColorChange}
+        >
+          Sign In
+        </animated.button>
+      </div>
+      {form === "register" && <Register setForm={setForm} />}
+      {form === "login" && <Login />}
     </>
   )
 }
