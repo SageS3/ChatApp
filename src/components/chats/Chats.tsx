341ca1a09c4124f771d00f6811e98160ed00740e
Justin Souza <justinsouza1029@gmail.com>
2023-05-17 23:54:30 -0700

    Delete last button- friends button from jsx

--- src/components/chats/Chats.tsx
+++ src/components/chats/Chats.tsx
@@ -1,7 +1,6 @@
 import { addGroup } from "../config/threadFunctions"
 import Threads from "./Threads"
 import { BiMessageSquareEdit } from "react-icons/bi"
-import { FaUserFriends } from "react-icons/fa"
 import "./chats.css"
 
 type ChatsProps = {
@@ -28,13 +27,9 @@ const Chats = ({ setDashboard, setThreadObj }: ChatsProps) => {
               <BiMessageSquareEdit size={"2.2em"} color={"rgb(77, 255, 148)"} />
             </button>
           </li>
-          <li>
-            <button className="button" onClick={() => setDashboard("friends")}>
-              <FaUserFriends size={"2.2em"} color={"rgb(77, 255, 148)"} />
-            </button>
-          </li>
         </ul>
       </header>
+
       <Threads setDashboard={setDashboard} setThreadObj={setThreadObj} />
     </div>
   )
