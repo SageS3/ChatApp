6866b90b3b0116b22f439f3feaf654d342f66fcc
Justin Souza <justinsouza1029@gmail.com>
2023-05-27 23:59:47 -0700

    Add function that handles manipulating multiple arrays in firbase

--- src/components/Friends/Friends.tsx
+++ src/components/Friends/Friends.tsx
@@ -3,6 +3,7 @@ import {
   collection,
   query,
   getDocs,
+  getDoc,
   doc,
   updateDoc,
   where,
@@ -14,18 +15,27 @@ import "./Friends.css"
 import { MdAdd } from "react-icons/md"
 import Requests from "./Requests"
 
-const Friends = () => {
-  const [allFriends, setAllFriends] = useState<any>([])
-  const [friendsDirectory, setFriendsDirectory] = useState<string>("all")
-  const [searchFriendQuery, setSearchFriendQuery] = useState<string>("")
-  const [searchFriendResults, setSearchFriendResults] = useState<any>([])
-  const [users, setUsers] = useState<any>([])
-  const user = auth?.currentUser
+type User = {
+  friends: {}
+  id: string
+  photoURL: string
+  userName: string
+}
 
-  useEffect(() => {
-    queryFriends(user)
-    console.log(searchFriendResults)
-  }, [])
+type FriendObj = {
+  id: string
+  userName: string
+  userPhoto: string
+}
+
+const Friends = () => {
+  const [friendsDirectory, setFriendsDirectory] = useState<string>("all")
+  const [pendingSentRequestsIDs, setPendingSentRequestsIDs] = useState<
+    string[]
+  >([])
+  const [pendingRequestsIDs, setPendingRequestsIDs] = useState<string[]>([])
+  const [users, setUsers] = useState<User[]>([])
+  const user = auth?.currentUser
 
   const queryFriends = async (user: any) => {
     const q = query(collection(db, "users"), where("id", "!=", `${user?.uid}`))
@@ -36,7 +46,28 @@ const Friends = () => {
     })
     setUsers(userList)
   }
-
+  const buttonState = (userID: string) => {
+    if (pendingSentRequestsIDs.includes(userID)) {
+      return <div className="pending-text">Pending...</div>
+    }
+    if (pendingRequestsIDs.includes(userID)) {
+      return (
+        <button>
+          <MdAdd size="1.8rem" color={"rgb(77, 255, 148)"} />
+        </button>
+      )
+    }
+    if (
+      !pendingRequestsIDs.includes(userID) &&
+      !pendingSentRequestsIDs.includes(userID)
+    ) {
+      return (
+        <button onClick={() => sendFriendRequestHandler(user)}>
+          <MdAdd size="1.8rem" color={"rgb(77, 255, 148)"} />
+        </button>
+      )
+    }
+  }
   const ListUsers = () => (
     <>
       {users.map((user: any) => (
@@ -45,10 +76,7 @@ const Friends = () => {
             <img src={user.photoURL} alt="" />
           </div>
           <p>{user.userName}</p>
-          {/* {user.Friends.pendingRequests} */}
-          <button onClick={() => sendFriendRequestHandler(user)}>
-            <MdAdd size="1.8rem" color={"rgb(77, 255, 148)"} />
-          </button>
+          {buttonState(user.id)}
         </div>
       ))}
     </>
@@ -91,6 +119,40 @@ const Friends = () => {
     await updateThirdPartyPendingRequests(requestedUser, currentUser)
   }
 
+  const getPendingUserRequestsIDs = async () => {
+    const currentUser = auth.currentUser
+    const userID = currentUser?.uid
+    const ref = doc(db, `users/${userID}`)
+    const docs = await getDoc(ref)
+
+    if (docs.exists()) {
+      const pendingSentRequestsArr = docs.data().friends.pendingSentRequests
+      const pendingRequestsArr = docs.data().friends.pendingRequests
+      if (pendingRequestsArr) {
+        const pendingRequestsIdArr = pendingRequestsArr.map(
+          (user: FriendObj) => {
+            return user.id
+          }
+        )
+        setPendingRequestsIDs(pendingRequestsIdArr)
+      }
+      if (pendingSentRequestsArr) {
+        const pendingSentRequestsIdArr = pendingSentRequestsArr.map(
+          (user: FriendObj) => {
+            return user.id
+          }
+        )
+        setPendingSentRequestsIDs(pendingSentRequestsIdArr)
+      }
+    } else {
+      console.log("doc not found")
+    }
+  }
+
+  useEffect(() => {
+    getPendingUserRequestsIDs()
+    queryFriends(user)
+  }, [])
   return (
     <div className="friends">
       <header className="friends__header">
