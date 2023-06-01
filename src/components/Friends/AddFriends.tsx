cd60fbbf7ba24c44fbad0a1b0eae56c943a41efe
Justin Souza <justinsouza1029@gmail.com>
2023-05-31 23:57:27 -0700

    delete functions that update documents in Firestore, object types and imports. Moved them to updateDocUtils file

--- src/components/Friends/AddFriends.tsx
+++ src/components/Friends/AddFriends.tsx
@@ -7,38 +7,33 @@ import {
   collection,
   where,
   getDocs,
-  updateDoc,
-  arrayUnion,
 } from "firebase/firestore"
 import { db } from "../config/firebase"
 import { MdAdd } from "react-icons/md"
-type User = {
-  friends: {}
-  id: string
-  photoURL: string
-  userName: string
-}
+import {
+  updateUserSentRequests,
+  updateThirdPartyPendingRequests,
+  acceptRequestFromListedUsers,
+  LimitedUserObj,
+  FullUserObj,
+} from "./updateDocUtils"
 
-type FriendObj = {
-  id: string
-  userName: string
-  userPhoto: string
-}
 const AddFriends = () => {
   const [pendingSentRequestsIDs, setPendingSentRequestsIDs] = useState<
     string[]
   >([])
   const [pendingRequestsIDs, setPendingRequestsIDs] = useState<string[]>([])
-  const [users, setUsers] = useState<User[]>([])
+  const [friendsIDs, setFriendsIDs] = useState<string[]>([])
+  const [users, setUsers] = useState<FullUserObj[]>([])
   const user = auth?.currentUser
+
   const sendFriendRequestHandler = async (requestedUser: any) => {
     const currentUser = auth?.currentUser
     await updateUserSentRequests(requestedUser, currentUser)
     await updateThirdPartyPendingRequests(requestedUser, currentUser)
-    await getPendingUserRequestsIDs()
+    await getUserIDs()
   }
   const queryUsers = async (user: any) => {
-    //
     const q = query(collection(db, "users"), where("id", "!=", `${user?.uid}`))
     const querySnapshot = await getDocs(q)
     const userList: any = []
@@ -47,37 +42,8 @@ const AddFriends = () => {
     })
     setUsers(userList)
   }
-  const updateThirdPartyPendingRequests = async (
-    //
-    requestedUser: any,
-    currentUser: any
-  ) => {
-    const thirdParyUserID = requestedUser.id
 
-    const ref = doc(db, "users", thirdParyUserID)
-    await updateDoc(ref, {
-      "friends.pendingRequests": arrayUnion({
-        userName: currentUser.displayName,
-        userPhoto: currentUser.photoURL,
-        id: currentUser.uid,
-      }),
-    })
-  }
-  const updateUserSentRequests = async (
-    requestedUser: any,
-    currentUser: any
-  ) => {
-    const userID = currentUser?.uid
-    const ref = doc(db, "users", userID)
-    await updateDoc(ref, {
-      "friends.pendingSentRequests": arrayUnion({
-        userName: requestedUser.userName,
-        userPhoto: requestedUser.photoURL,
-        id: requestedUser.id,
-      }),
-    })
-  }
-  const getPendingUserRequestsIDs = async () => {
+  const getUserIDs = async () => {
     const currentUser = auth.currentUser
     const userID = currentUser?.uid
     const ref = doc(db, `users/${userID}`)
@@ -86,9 +52,10 @@ const AddFriends = () => {
     if (docs.exists()) {
       const pendingSentRequestsArr = docs.data().friends.pendingSentRequests
       const pendingRequestsArr = docs.data().friends.pendingRequests
+      const friendsArr = docs.data().friends.friends
       if (pendingRequestsArr) {
         const pendingRequestsIdArr = pendingRequestsArr.map(
-          (user: FriendObj) => {
+          (user: LimitedUserObj) => {
             return user.id
           }
         )
@@ -96,27 +63,39 @@ const AddFriends = () => {
       }
       if (pendingSentRequestsArr) {
         const pendingSentRequestsIdArr = pendingSentRequestsArr.map(
-          (user: FriendObj) => {
+          (user: LimitedUserObj) => {
             return user.id
           }
         )
         setPendingSentRequestsIDs(pendingSentRequestsIdArr)
+      }
+      if (friendsArr) {
+        const friendsIdArr = friendsArr.map((user: LimitedUserObj) => {
+          return user.id
+        })
+        setFriendsIDs(friendsIdArr)
       }
     } else {
       console.log("doc not found")
     }
   }
 
-  const buttonState = (userID: string, userObj: User) => {
+  const buttonState = (userID: string, userObj: FullUserObj) => {
     //
     if (pendingSentRequestsIDs.includes(userID)) {
       return <div className="pending-text">Pending...</div>
     }
+    if (friendsIDs.includes(userID)) {
+      return <div className="button-state__friends">Friends</div>
+    }
     if (pendingRequestsIDs.includes(userID)) {
       return (
-        <button onClick={() => addFriendHandler(userObj)}>
-          <MdAdd size="1.8rem" color={"rgb(77, 255, 148)"} />
-        </button>
+        <div>
+          <button onClick={() => acceptRequestFromListedUsers(userObj)}>
+            Accept
+          </button>
+          <button type="button">Ignore</button>
+        </div>
       )
     }
     if (
@@ -131,9 +110,8 @@ const AddFriends = () => {
     }
   }
   const ListUsers = () => (
-    //
     <>
-      {users.map((user: User) => (
+      {users.map((user: FullUserObj) => (
         <div key={user.id} className="user-container">
           <div className="user-image-container">
             <img src={user.photoURL} alt="" />
@@ -146,15 +124,9 @@ const AddFriends = () => {
     </>
   )
 
-  const addFriendHandler = (user: User) => {
-    // from the pendingRequests array, the listed user click event will add that user's obj to the friends arr in Firebase
-    // this function will also handle adding the current user Obj to that selected user's friends arr in Firebase.
-    // resulting in the request being accepted
-    console.log(user)
-  }
   useEffect(() => {
-    getPendingUserRequestsIDs() //
-    queryUsers(user) //
+    getUserIDs()
+    queryUsers(user)
   }, [])
   return (
     <>
