e0e00a32985244b1b740b9179a78a4b8c5908c5c
Justin Souza <justinsouza1029@gmail.com>
2023-05-31 23:45:37 -0700

    Add all functions used in the Friends folder that updates a document in Firestore

---
+++ src/components/Friends/updateDocUtils.tsx
@@ -0,0 +1,106 @@
+import { doc, updateDoc, arrayUnion, arrayRemove } from "firebase/firestore"
+import { db } from "../config/firebase"
+import { auth } from "../config/firebase"
+
+export type LimitedUserObj = {
+  id: string
+  userName: string
+  userPhoto: string
+}
+
+export type FullUserObj = {
+  friends: {}
+  id: string
+  photoURL: string
+  userName: string
+}
+
+export const updateUserSentRequests = async (
+  requestedUser: any,
+  currentUser: any
+) => {
+  const userID = currentUser?.uid
+  const ref = doc(db, "users", userID)
+  await updateDoc(ref, {
+    "friends.pendingSentRequests": arrayUnion({
+      userName: requestedUser.userName,
+      userPhoto: requestedUser.photoURL,
+      id: requestedUser.id,
+    }),
+  })
+}
+
+export const updateThirdPartyPendingRequests = async (
+  requestedUser: any,
+  currentUser: any
+) => {
+  const thirdParyUserID = requestedUser.id
+  const ref = doc(db, "users", thirdParyUserID)
+  await updateDoc(ref, {
+    "friends.pendingRequests": arrayUnion({
+      userName: currentUser.displayName,
+      userPhoto: currentUser.photoURL,
+      id: currentUser.uid,
+    }),
+  })
+}
+
+export const acceptRequestFromListedRequests = async (
+  requester: LimitedUserObj
+) => {
+  const currentUser = auth?.currentUser
+  const currentUserID = currentUser?.uid
+  const currentUserRef = doc(db, `users/${currentUserID}`)
+  const requesterRef = doc(db, `users/${requester.id}`)
+  await updateDoc(currentUserRef, {
+    "friends.friends": arrayUnion({
+      userName: requester.userName,
+      userPhoto: requester.userPhoto,
+      id: requester.id,
+    }),
+    "friends.pendingRequests": arrayRemove({
+      userName: requester.userName,
+      userPhoto: requester.userPhoto,
+      id: requester.id,
+    }),
+  })
+  updateCurrentUserDocs(currentUser, requesterRef)
+}
+
+export const acceptRequestFromListedUsers = async (requester: FullUserObj) => {
+  const currentUser = auth?.currentUser
+  const currentUserID = currentUser?.uid
+  const currentUserRef = doc(db, `users/${currentUserID}`)
+  const requesterRef = doc(db, `users/${requester.id}`)
+
+  await updateDoc(currentUserRef, {
+    "friends.friends": arrayUnion({
+      userName: requester.userName,
+      userPhoto: requester.photoURL,
+      id: requester.id,
+    }),
+    "friends.pendingRequests": arrayRemove({
+      userName: requester.userName,
+      userPhoto: requester.photoURL,
+      id: requester.id,
+    }),
+  })
+  updateCurrentUserDocs(currentUser, requesterRef)
+}
+
+const updateCurrentUserDocs = async (currentUser: any, requesterRef: any) => {
+  if (currentUser) {
+    await updateDoc(requesterRef, {
+      "friends.friends": arrayUnion({
+        userName: currentUser.displayName,
+        userPhoto: currentUser.photoURL,
+        id: currentUser.uid,
+      }),
+      "friends.pendingSentRequests": arrayRemove({
+        userName: currentUser.displayName,
+        userPhoto: currentUser.photoURL,
+        id: currentUser.uid,
+      }),
+    })
+  }
+}
