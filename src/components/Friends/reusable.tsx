1dd16893a4e397e482c0f3f9105b356f50e7a9af
Justin Souza <justinsouza1029@gmail.com>
2023-06-02 12:02:17 -0700

    Update AcceptIgnoreButtons component to accept two functions and a user object

--- src/components/Friends/reusable.tsx
+++ src/components/Friends/reusable.tsx
@@ -1,4 +1,24 @@
-export const AcceptIgnoreButtons = () => {
-  // params: accept function, ignore function
-  return <div>button</div>
+import { LimitedUserObj, FullUserObj } from "./updateDocUtils"
+
+type AcceptIgnoreButtonsProps = {
+  accept: (user: any) => {}
+  ignore: (user: any) => {}
+  userObj: LimitedUserObj | FullUserObj
+}
+
+export const AcceptIgnoreButtons = ({
+  accept,
+  ignore,
+  userObj,
+}: AcceptIgnoreButtonsProps) => {
+  return (
+    <div className="accept-ignore__buttons">
+      <button type="button" onClick={() => accept(userObj)}>
+        Accept
+      </button>
+      <button type="button" onClick={() => ignore(userObj)}>
+        Ignore
+      </button>
+    </div>
+  )
 }
