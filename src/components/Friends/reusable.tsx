86acdbe8cf72f011e49517ac4a46dd6144b39904
Justin Souza <justinsouza1029@gmail.com>
2023-06-03 23:56:59 -0700

    Add MappedUsers component that will be reusable

--- src/components/Friends/reusable.tsx
+++ src/components/Friends/reusable.tsx
@@ -1,4 +1,5 @@
 import { LimitedUserObj, FullUserObj } from "./updateDocUtils"
+import { BiMessageSquareEdit } from "react-icons/bi"
 
 type AcceptIgnoreButtonsProps = {
   accept: (user: any) => {}
@@ -22,3 +23,28 @@ export const AcceptIgnoreButtons = ({
     </div>
   )
 }
+
+type MappedUserProps = {
+  userArr: LimitedUserObj[] | FullUserObj[]
+  buttonState?: (a: string, b: FullUserObj) => JSX.Element
+}
+
+const fullUserObjTypeCheck = (
+  userArr: LimitedUserObj | FullUserObj
+): userArr is FullUserObj => {
+  return true
+}
+
+export const MappedUsers = ({ userArr, buttonState }: MappedUserProps) => (
+  <>
+    {userArr.map((user: LimitedUserObj | FullUserObj) => (
+      <div className="user-container" key={user.id}>
+        <div className="user-image-container">
+          <img src={user.photoURL} />
+        </div>
+        <p>{user.userName}</p>
+        {/* {fullUserObjTypeCheck(user) && buttonState(user.id, user)} */}
+      </div>
+    ))}
+  </>
+)
