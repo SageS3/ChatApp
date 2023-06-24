c96ee328d8c0de3c9d57c08087c8eedef8ab3124
Justin Souza <justinsouza1029@gmail.com>
2023-06-23 22:07:25 -0700

    Add query functions and state the updates the requests, friends and users

--- src/components/Friends/Friends.tsx
+++ src/components/Friends/Friends.tsx
@@ -1,36 +1,99 @@
 import { useEffect, useState } from "react"
-import { getDoc, doc } from "firebase/firestore"
+import {
+  getDoc,
+  doc,
+  collection,
+  where,
+  getDocs,
+  query,
+} from "firebase/firestore"
 import { db } from "../config/firebase"
 import { auth } from "../config/firebase"
 import "./Friends.css"
 import Requests from "./Requests"
 import AddFriends from "./AddFriends"
-import { LimitedUserObj } from "./updateDocUtils"
+import { LimitedUserObj, FullUserObj } from "./updateDocUtils"
 import { MappedUsers } from "./reusable"
 
 const Friends = () => {
   const [friendsDirectory, setFriendsDirectory] = useState<string>("all")
-  const [friends, setFriends] = useState<LimitedUserObj[]>([])
-  const [hasRequests, setHasRequests] = useState<boolean>(false)
+  const [friends, setFriends] = useState<FullUserObj[]>([])
+  const [users, setUsers] = useState<FullUserObj[]>([])
+  const [requests, setRequests] = useState<FullUserObj[]>([])
+  const [requestIDs, setRequestIDs] = useState<string[]>([])
+  const [friendIDs, setFriendIDs] = useState<string[]>([])
+
+  const queryUsers = async (user: any) => {
+    const q = query(collection(db, "users"), where("id", "!=", `${user?.uid}`))
+    const querySnapshot = await getDocs(q)
+    const userList: any = []
+    querySnapshot.forEach((user: any) => {
+      userList.push(user.data())
+    })
+    setUsers(userList)
+  }
+
+  const queryRequests = async () => {
+    const currentUser = auth.currentUser
+    const userID = currentUser?.uid
+    const ref = doc(db, `users/${userID}`)
+    const querySnapshot = await getDoc(ref)
+    const requestsArr: string[] = []
+    if (querySnapshot.exists()) {
+      const pendingRequests = querySnapshot.data().friends.pendingRequests
+      pendingRequests.forEach((userObj: LimitedUserObj) => {
+        requestsArr.push(userObj.id)
+      })
+    } else {
+      console.log("no requests")
+    }
+    setRequestIDs(requestsArr)
+  }
+
   const queryFriends = async () => {
     const currentUser = auth.currentUser
     const userID = currentUser?.uid
     const ref = doc(db, `users/${userID}`)
     const querySnapshot = await getDoc(ref)
-    const friendsObjArr: LimitedUserObj[] = []
+    const friendsObjArr: string[] = []
     if (querySnapshot.exists()) {
       const friendsArr = querySnapshot.data().friends.friends
       friendsArr.forEach((userObj: LimitedUserObj) => {
-        friendsObjArr.push(userObj)
+        friendsObjArr.push(userObj.id)
       })
     } else {
       console.log("no requests")
     }
-    setFriends(friendsObjArr)
-    console.log(friends)
+    setFriendIDs(friendsObjArr)
+  }
+
+  const populateRequests = (users: FullUserObj[], requestIDs: string[]) => {
+    const reqs: FullUserObj[] = []
+    requestIDs.forEach((id: string) => {
+      const filtered = users.filter((user: FullUserObj) => {
+        return user.id === id
+      })
+      reqs.push(...filtered)
+    })
+    setRequests(reqs)
+  }
+
+  const populateFriends = (users: FullUserObj[], friendIDs: string[]) => {
+    const friendsArr: FullUserObj[] = []
+    friendIDs.forEach((id: string) => {
+      const filtered = users.filter((user: LimitedUserObj) => {
+        return user.id === id
+      })
+      friendsArr.push(...filtered)
+    })
+    setFriends(friendsArr)
   }
 
   useEffect(() => {
+    const currentUser = auth?.currentUser
+    queryUsers(currentUser)
+    populateRequests(users, requestIDs)
+    populateFriends(users, friendIDs)
     queryFriends()
   }, [])
 
@@ -44,7 +107,7 @@ const Friends = () => {
     <button
       className={selected ? "selected" : ""}
       onClick={onClick}
-      id={id === "Requests" && hasRequests ? "requests" : ""}
+      id={id === "Requests" ? "requests" : ""}
     >
       {id}
     </button>
@@ -84,9 +147,14 @@ const Friends = () => {
       </header>
       <main className="friends--list">
         {friendsDirectory === "all" && <MappedUsers userArr={friends} />}
-        {friendsDirectory === "add" && <AddFriends />}
+        {friendsDirectory === "add" && <AddFriends users={users} />}
         {friendsDirectory === "requests" && (
-          <Requests setHasRequests={setHasRequests} />
+          <Requests
+            requestIDs={requestIDs}
+            requests={requests}
+            setRequestIDs={setRequestIDs}
+            queryRequests={queryRequests}
+          />
         )}
       </main>
     </div>
