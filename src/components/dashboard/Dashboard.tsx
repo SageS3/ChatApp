8d70761204f3b43108ed2e6046ee855d9b400015
Justin Souza <justinsouza1029@gmail.com>
2023-08-03 15:00:19 -0700

    updat profile icon with user image

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -120,13 +120,13 @@ const Dashboard = () => {
   return (
     <div className="user-dashboard">
       <nav className="navbar__tablet--display">
+        <button onClick={() => setDashboard(navigateTo.profile)}>
+          <img src={userPhoto} />
+        </button>
         <button onClick={() => setDashboard(navigateTo.chats)}>
           <AiFillMessage size={"2.2em"} color={"rgb(39 194 160)"} />
         </button>
-        <button onClick={() => setDashboard(navigateTo.profile)}>
-          <FaUser size={"2.2em"} color={"rgb(39 194 160)"} />
-        </button>
-        <button>
+        <button onClick={() => setDashboard(navigateTo.settings)}>
           <IoSettingsSharp size={"2.2em"} color={"rgb(39 194 160)"} />
         </button>
         <button className="logout-nav-button" onClick={handleLogOut}>
