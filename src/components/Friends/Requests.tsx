605b410aa980c00b075caf0f9c5742315d4ebfe1
Justin Souza <justinsouza1029@gmail.com>
2023-09-29 12:52:49 -0700

    Update accept and ignore requests functions to filter requests based on the updated ids and request state, wrapped in a try catch block and some loading state so the UI changes accordingly

--- src/components/Friends/Requests.tsx
+++ src/components/Friends/Requests.tsx
@@ -12,64 +12,109 @@ import {
 import { MappedUsers } from "./reusable"
 
 type RequestsProps = {
+  filterRequests: (users: FullUserObj[], idArr: string[]) => FullUserObj[]
   requestIDs: string[]
   setRequestIDs: any
   requests: FullUserObj[]
   setRequests: any
+  setIsLoading: any
+  isLoading: any
 }
 
-const Requests = ({ requestIDs, requests, setRequestIDs }: RequestsProps) => {
+const Requests = ({
+  requestIDs,
+  requests,
+  setRequests,
+  setRequestIDs,
+  filterRequests,
+  setIsLoading,
+  isLoading,
+}: RequestsProps) => {
   const ignoreRequest = async (requester: LimitedUserObj) => {
+    // updates Firestore:
+    // removes requester from pendingRequests.
+    // removes requester's pendingSentRequests data.
+
     const currentUser = auth?.currentUser
     const currentUserID = currentUser?.uid
     const currentUserRef = doc(db, `users/${currentUserID}`)
     const requesterRef = doc(db, `users/${requester.id}`)
-    await updateDoc(currentUserRef, {
-      "friends.pendingRequests": arrayRemove({
-        id: requester.id,
-      }),
-    })
-    if (currentUser) {
-      await updateDoc(requesterRef, {
-        "friends.pendingSentRequests": arrayRemove({
-          id: currentUser.uid,
+    try {
+      setIsLoading(true)
+      await updateDoc(currentUserRef, {
+        "friends.pendingRequests": arrayRemove({
+          id: requester.id,
         }),
       })
+      if (currentUser) {
+        await updateDoc(requesterRef, {
+          "friends.pendingSentRequests": arrayRemove({
+            id: currentUser.uid,
+          }),
+        })
+      }
+
+      const updateRequestIds = requestIDs.filter(
+        (request: string) => request !== requester.id
+      )
+      setRequestIDs(updateRequestIds)
+      const result = filterRequests(requests, updateRequestIds)
+      setRequests(result)
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setIsLoading(false)
     }
-    const updateRequests = requestIDs.filter(
-      (request: string) => request !== requester.id
-    )
-    setRequestIDs(updateRequests)
   }
 
   const acceptRequestFromListedRequests = async (requester: LimitedUserObj) => {
+    // updates Firestore:
+    // adds requester as friends.
+    // removes requester from requests array.
+
     const currentUser = auth?.currentUser
     const currentUserID = currentUser?.uid
     const currentUserRef = doc(db, `users/${currentUserID}`)
     const requesterRef = doc(db, `users/${requester.id}`)
-    await updateDoc(currentUserRef, {
-      "friends.friends": arrayUnion({
-        id: requester.id,
-      }),
-      "friends.pendingRequests": arrayRemove({
-        id: requester.id,
-      }),
-    })
-    updateCurrentUserDocs(currentUser, requesterRef)
-    const updateRequests = requestIDs.filter(
-      (request: string) => request !== requester.id
-    )
-    setRequestIDs(updateRequests)
+
+    try {
+      setIsLoading(true)
+      await updateDoc(currentUserRef, {
+        "friends.friends": arrayUnion({
+          id: requester.id,
+        }),
+        "friends.pendingRequests": arrayRemove({
+          id: requester.id,
+        }),
+      })
+
+      updateCurrentUserDocs(currentUser, requesterRef)
+
+      const updateRequestIds = requestIDs.filter(
+        (request: string) => request !== requester.id
+      )
+      setRequestIDs(updateRequestIds)
+      const result = filterRequests(requests, updateRequestIds)
+      setRequests(result)
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setIsLoading(false)
+    }
   }
 
   return (
     <div className="main__requests">
-      <MappedUsers
-        userArr={requests}
-        AcceptIgnoreButtons={AcceptIgnoreButtons}
-        accept={acceptRequestFromListedRequests}
-        ignore={ignoreRequest}
-      />
+      {isLoading ? (
+        <div>...loading</div>
+      ) : (
+        <MappedUsers
+          userArr={requests}
+          AcceptIgnoreButtons={AcceptIgnoreButtons}
+          accept={acceptRequestFromListedRequests}
+          ignore={ignoreRequest}
+        />
+      )}
     </div>
   )
 }
